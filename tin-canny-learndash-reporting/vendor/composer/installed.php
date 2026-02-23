018ef159fa0ffc09f7f2bc7487b70d350d1d7a39
AyoubNarhach <ayoub.narhach@gmail.com>
2026-02-23 03:07:25 +0000

    Plugin initial

---
+++ tin-canny-learndash-reporting/vendor/composer/installed.php
@@ -0,0 +1,50 @@
+<?php return array(
+    'root' => array(
+        'name' => 'uncannyowl/tin-canny-learndash-reporting',
+        'pretty_version' => '5.1.1.x-dev',
+        'version' => '5.1.1.9999999-dev',
+        'reference' => 'a85ba10ffa31f0dd7ee3934033dc28ab6c700607',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'namshi/jose' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'opigno/tincan' => array(
+            'pretty_version' => '1.1.4',
+            'version' => '1.1.4.0',
+            'reference' => 'cf662853a45b7ba6b98e2359527e573d3ad75808',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../opigno/tincan',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rusticisoftware/tincan' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'uncannyowl/tin-canny-learndash-reporting' => array(
+            'pretty_version' => '5.1.1.x-dev',
+            'version' => '5.1.1.9999999-dev',
+            'reference' => 'a85ba10ffa31f0dd7ee3934033dc28ab6c700607',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'willdurand/negotiation' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
